65cfe97410d04ea2fc8e57bf867caf3e52117bb4
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2025-01-13 20:28:11 +0530

    Added the function addTodo simple dom usage shown by adding the elements in the page using dom functionality.

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 console.log("Welcome to the Todo App.")
 
-let getTodo = document.getElementById("get-todo");
+let todoDataSection = document.getElementsByClassName("todo-data"); //This is extract the html element where we need to at the end append our todo child.
+function addTodo(todoData) {
+    let rowDiv = document.createElement("div");
+    let todoItem = document.createElement("div");
+    let todoNumber = document.createElement("div");
+    let todoDetail = document.createElement("div");
+    let todoStatus = document.createElement("div");
+    let todoAction = document.createElement("div");
+    let deleteButton = document.createElement("button");
+    let finishedButton = document.createElement("button");
+    let hr = document.createElement("hr");
+
+    todoNumber.textContent = "1";
+    todoDetail.textContent = todoData; //This text content will get that value which we will pass by pressing the save of the input.
+    todoStatus.textContent = "In Progress";
+    deleteButton.textContent = "Delete";
+    finishedButton.textContent = "Finished";
+
+    //Now inside the todo-action div we will append our delete and finished button
+    todoAction.appendChild(deleteButton);
+    todoAction.appendChild(finishedButton);
+
+    //Just appending the elements
+    todoItem.appendChild(todoNumber);
+    todoItem.appendChild(todoDetail);
+    todoItem.appendChild(todoStatus);
+    todoItem.appendChild(todoAction);
+
+    rowDiv.appendChild(todoItem);
+
+    //Now appending our todo in the todo-data div.
+    todoDataSection.appendChild(rowDiv);
+
+}
+
+
+
+
+
+
+
+
+//For Reference ->
+
+// let getTodo = document.getElementById("get-todo");
 
 //This is technically a registration of the event Listener and we can attach mutiple listener.
 // getTodo.addEventListener("click", ()=>{
@@ -20,7 +64,7 @@ let getTodo = document.getElementById("get-todo");
 
 //Third way of adding an event is by directly attaching the event type in the HTML element with the help of an attribute.
 //And here we define the function which will happen when that event is triggered.
-function clickBtn() {
-    getTodo.style.display = "none";
-    console.log("Clicked the get-todo and The display property is set to Hidden");
-}
+// function clickBtn() {
+//     getTodo.style.display = "none";
+//     console.log("Clicked the get-todo and The display property is set to Hidden");
+// }
