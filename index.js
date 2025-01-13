c2e7d07b60d2bbb40772d6b955013a308cc8f933
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2025-01-13 21:43:33 +0530

    Added the save btn logic and it is working absolutely fine with UI display.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 console.log("Welcome to the Todo App.")
 
 let todoDataSection = document.getElementsByClassName("todo-data"); //This is extract the html element where we need to at the end append our todo child.
+let saveBtn = document.getElementById("save-btn");
+let todoInputBar = document.getElementById("todo-input-bar");
+
+//Adding a listener to tell if the anything is written in the input bar then enable the save button.
+todoInputBar.addEventListener("keyup", function toggleSaveBtn() {
+    if(todoInputBar.value.length == 0) { //If the length of the text in the input bar is 0 then check if the save btn doesn't contains disabled then add it othewise don't do anything.
+        if(saveBtn.classList.contains("disabled"))  return;
+        saveBtn.classList.add("disabled");
+    }
+    else if(saveBtn.classList.contains("disabled"))
+        saveBtn.classList.remove("disabled");
+    //If we have runned if once then we don't need to do anything else therefore other condition is in else if
+    //if the text length is not zero and if the save btn contains disabled property then we need to remove that property.
+});
+
+saveBtn.addEventListener("click", function getTextAndAddTodo() {
+    let todoText = todoInputBar.value; //With the value property we can access the text present inside our input bar.
+    if(todoText.length == 0)    return; //If there is no todo passed then we don't need to do anything.
+    addTodo(todoText); //If there is some value available in the todo bar then we need to call the addTodo function with the text in it.
+    todoInputBar.value = ""; //Once we have added the todo by clicking on the save button the input bar should go blank once again.
+});
+
 function addTodo(todoData) {
     let rowDiv = document.createElement("div");
     let todoItem = document.createElement("div");
